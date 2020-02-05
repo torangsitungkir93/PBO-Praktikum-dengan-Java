9bf6c8a8a91043278915ebdf3a310aa0a1a48061
torangsitungkir93 <torangsitungkir93@gmail.com>
2020-02-06 00:08:31 +0700

    Mengedit Komentar

--- Tugas1PBO/Tugas1/src/Main.java
+++ Tugas1PBO/Tugas1/src/Main.java
@@ -1,21 +1,11 @@
 
 import java.util.Scanner;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author asus
+ * @author torangto Situngkir
  */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
        String nama;
        int nilai[]= new int[5];
